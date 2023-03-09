bf89377f4c4d4aa1afedf87de5965e1eea9ca6fc
Will Childs-Klein <childw@amazon.com>
2023-03-09 15:07:54 -0500

    Revert "Add line to env setup to check AWS-LC version"
    This reverts commit 297f1d8fc25c2f3ce1c331971e3e58cb2bfe4bfd.

--- codebuild/bin/s2n_setup_env.sh
+++ codebuild/bin/s2n_setup_env.sh
@@ -135,13 +135,10 @@ if [[ "$S2N_LIBCRYPTO" == "openssl-1.0.2-fips" ]]; then
 fi
 if [[ "$S2N_LIBCRYPTO" == "boringssl" ]]; then export LIBCRYPTO_ROOT=$BORINGSSL_INSTALL_DIR ; fi
 
-if [[ "$S2N_LIBCRYPTO" =~ "awslc" ]]; then
-    if [[ "$S2N_LIBCRYPTO" == "awslc" ]]; then export LIBCRYPTO_ROOT=$AWSLC_INSTALL_DIR ; fi
-    if [[ "$S2N_LIBCRYPTO" == "awslc-fips" ]]; then
-      export LIBCRYPTO_ROOT=$AWSLC_FIPS_INSTALL_DIR ;
-      export S2N_TEST_IN_FIPS_MODE=1 ;
-    fi
-    echo "AWS-LC version: $(cat $LIBCRYPTO_ROOT/bin/bssl* | grep -aoP 'AWS-LC( FIPS)? [\d]+\.[\d]+\.[\d]+')" ;
+if [[ "$S2N_LIBCRYPTO" == "awslc" ]]; then export LIBCRYPTO_ROOT=$AWSLC_INSTALL_DIR ; fi
+if [[ "$S2N_LIBCRYPTO" == "awslc-fips" ]]; then
+  export LIBCRYPTO_ROOT=$AWSLC_FIPS_INSTALL_DIR ;
+  export S2N_TEST_IN_FIPS_MODE=1 ;
 fi
 
 if [[ "$S2N_LIBCRYPTO" == "libressl" ]]; then export LIBCRYPTO_ROOT=$LIBRESSL_INSTALL_DIR ; fi
